3ca1eeb69ee920941619ca2fb68149f51fba9c58
oelassri <elassri.omar@gmail.com>
2022-02-20 19:09:32 +0100

    addB

--- src/Test.java
+++ src/Test.java
@@ -4,8 +4,13 @@ public class Test {
 	public int addM(int a, int b) {
 		return a+b;
 	}
+<<<<<<< HEAD
 	public int multM(int a, int b) {
 		return a*b;
+=======
+	public int addB(int a, int b) {
+		return a+b;
+>>>>>>> 19d1644 (addB)
 	}
 
 }
